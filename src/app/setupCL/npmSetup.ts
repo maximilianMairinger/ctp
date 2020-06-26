53b317c403c688d1227d8757b4c42b2b7c290ddd
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-27 01:25:29 +0200

    npm publish before install

--- src/app/setupCL/npmSetup.ts
+++ src/app/setupCL/npmSetup.ts
@@ -2,11 +2,11 @@ import exec, { check } from "./shell"
 
 export default async function(dependencies: string[], publish: boolean = false) {
   check("npm")
+
+  if (publish) exec("npm publish")
   
   exec("npm i")
   dependencies.ea((dependency) => {
     exec("npm i " + dependency)
   })
-
-  if (publish) exec("npm publish")
 }
