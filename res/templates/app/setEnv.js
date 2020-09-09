8aab36576d987a844feeba8f45b189edd35e0407
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-09 23:08:05 +0200

    templ/app: setEnv fix

--- res/templates/app/setEnv.js
+++ res/templates/app/setEnv.js
@@ -4,7 +4,7 @@ const preset = agrs.preset ? agrs.preset : "repl"
 const fs = require("fs")
 const editJson = require("edit-json-file")
 
-fs.writeFileSync(".domain", preset)
+fs.writeFileSync(".deploy", preset)
 let file = editJson("package.json")
 file.set("version", "1.0.0-" + preset)
 file.save()
