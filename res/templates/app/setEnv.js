1cbd6f5e66a4e59e7065b7efaf2c738683c8714b
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-09 22:59:24 +0200

    templ/app: setEnv command implemented

---
+++ res/templates/app/setEnv.js
@@ -0,0 +1,10 @@
+const yargs = require("yargs")
+const agrs = yargs.argv
+const preset = agrs.preset ? agrs.preset : "repl"
+const fs = require("fs")
+const editJson = require("edit-json-file")
+
+fs.writeFileSync(".domain", preset)
+let file = editJson("package.json")
+file.set("version", "1.0.0-" + preset)
+file.save()
