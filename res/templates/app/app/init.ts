6794d3e5e3d42a3ec4844eef4d2e8a75c1260b97
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-22 18:48:10 +0200

    dont install sw by default

--- res/templates/app/app/init.ts
+++ res/templates/app/app/init.ts
@@ -10,18 +10,18 @@ export async function init() {
   main()
 
 
-  if ("serviceWorker" in navigator) {
-    if (navigator.serviceWorker.controller) {
-      console.log("[SW] Found Service worker")
-    } else {
-      await navigator.serviceWorker.register("./sw.js", {scope: "./"}).then(function(reg){
-        console.log("[SW] Service worker installed with scope: " + reg.scope)
-      })
-    }
-  }
-  else {
-    console.warn("[SW] Unable to install Service worker. Not supported.");
-  }
+  // if ("serviceWorker" in navigator) {
+  //   if (navigator.serviceWorker.controller) {
+  //     console.log("[SW] Found Service worker")
+  //   } else {
+  //     await navigator.serviceWorker.register("./sw.js", {scope: "./"}).then(function(reg){
+  //       console.log("[SW] Service worker installed with scope: " + reg.scope)
+  //     })
+  //   }
+  // }
+  // else {
+  //   console.warn("[SW] Unable to install Service worker. Not supported.");
+  // }
 }
   
   
