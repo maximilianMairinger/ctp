d9be4224a4b053ad74cac29a0468e2ef735fd383
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-17 13:50:34 +0100

    testing infrastructure

---
+++ res/templates/module/test/src/test.ts
@@ -0,0 +1,11 @@
+import { test } from "shelljs"
+import { describe } from "yargs"
+import $[name] from "../../app/src/$[name]"
+
+describe("core", () => {
+  
+  test("mock", () => {
+
+  })
+  
+})
