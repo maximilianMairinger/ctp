7671b5032cc96a80c8fbdbc9c099454ab4ed65ad
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-06-20 12:23:46 +0200

    update component

--- res/templates/app/app/_component/component.ts
+++ res/templates/app/app/_component/component.ts
@@ -1,83 +1,97 @@
+import "../global"
 import lang from "./../lib/lang"
 import { DataBase, Data } from "josm";
+import { ElementList, ElementListOrElement, PrimElem, VariableLibrary } from "extended-dom";
 
-type PrimElem = string | number | boolean | Element
 type Token = string | string[]
 
-const compCss = require('./component.css')
-
-
-export default abstract class Component extends HTMLElement {
+export default abstract class Component<T extends HTMLElement | HTMLAnchorElement | false | never = HTMLElement> extends HTMLElement {
   protected sr: ShadowRoot;
-  protected componentBody: HTMLElement
+  protected componentBody: T extends (HTMLElement | HTMLAnchorElement) ? T : T extends false ? ShadowRoot : HTMLElement
 
-  constructor(componentBodyExtention?: HTMLElement | false) {
+
+  constructor(bodyExtension?: T) {
     super();
     this.sr = this.attachShadow({mode: "open"});
 
-    let thisStl = this.stl()
-    let thisPug = this.pug() as any
-    if (typeof thisPug === "object") {
-      if (typeof thisPug.default === "string") thisPug = thisPug.default
-      else thisPug = thisPug.toString()
-    }
-
     
-    if (componentBodyExtention !== false) {
-      if (componentBodyExtention !== undefined) {
-        //@ts-ignore
-        this.componentBody = componentBodyExtention
-      }
-      else this.componentBody = ce("component-body")
+    if (bodyExtension !== false) {
+      //@ts-ignore
+      this.componentBody = bodyExtension === undefined ? ce("component-body") : bodyExtension
 
 
-      this.sr.html("<!--General styles--><style>" + compCss + "</style><!--Main styles--><style>" + thisStl + "</style>")
-      this.sr.append(this.componentBody)
-      this.componentBody.html(thisPug, lang)
+      this.sr.html("<!--General styles--><style>" + require('./component.css').toString() + "</style><!--Main styles--><style>" + this.stl() + "</style>")
+      this.componentBody.html(this.pug(), lang)
+      this.sr.append(this.componentBody as HTMLElement)
     }
     else {
       //@ts-ignore
       this.componentBody = this.sr
-      this.sr.html("<!--General styles--><style>" + compCss + "</style><!--Main styles--><style>" + thisStl + "</style>").apd(thisPug, lang)
+      this.sr.html("<!--General styles--><style>" + require('./component.css').toString() + "</style><!--Main styles--><style>" + this.stl() + "</style>").apd(this.pug(), lang)
     }
-
   }
 
   protected attributeChangedCallback(attrName: string, oldVal: string, newVal: string) {
     this[attrName](newVal)
   }
 
-  public q(qs?: string) {
-    return this.componentBody.childs(qs)
+  public abstract stl(): string;
+  public abstract pug(): string;
+  /**
+   * Appends to ShadowRoot
+   */
+  public sra(...elems: HTMLElement[]): void {
+    elems.ea((e) => {
+      this.sr.append(e);
+    });
   }
 
 
+  /**
+   * Gets children matching given css-selector
+   * @param selector css-selector filter childs similar to document.querySelector
+   * @param alwaysReturnElementList when true, always return a ELementList instead of defaulting to a single instance when the query does only math once (defaults to false)
+   */
+  public q(selector: string, alwaysReturnElementList: true): ElementList<Element>
+  /**
+   * Gets children matching given css-selector
+   * @param selector css-selector filter childs similar to document.querySelector
+   * @param alwaysReturnElementList when true, always return a ELementList instead of defaulting to a single instance when the query does only math once (defaults to false)
+   */
+  public q(selector: string, alwaysReturnElementList?: boolean): ElementListOrElement
+  public q(qs?: string, alwaysReturnElementList?: boolean) {
+    return this.componentBody.childs(qs, alwaysReturnElementList)
+  }
   public apd(...elems: PrimElem[]): this
-  public apd(to: PrimElem | PrimElem[], library?: {[key in string]: string | Data<string>;} | DataBase, customTokens?: {open?: Token; close?: Token; escape?: Token;}): this
+  public apd(to: PrimElem | PrimElem[], library?: VariableLibrary, customTokens?: {open?: Token, close?: Token, escape?: Token}): this
   public apd(...a: any): this {
     this.componentBody.apd(...a)
     return this
   }
 
-
-  public stl?(): string;
-  public pug?(): string | object;
+  protected parseJSONProp(prop: any) {
+    if (typeof prop === "string") return JSON.parse(prop)
+    else return prop
+  }
 }
 
 
 /*
-import Component from "./../component";
+import Element from "../element"
 
-export default class Example extends Component {
+export default class Example extends Element {
+
   constructor() {
     super()
 
+
   }
+
   stl() {
     return require("./example.css").toString()
   }
   pug() {
-    return require("./example.pug").toString()
+    return require("./example.pug").default
   }
 }
 
