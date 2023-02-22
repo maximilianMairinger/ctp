8bbcfd01bc5ff848e622cad22875111be1d734e2
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-02-22 02:58:23 +0100

    mod: fix build and no longer cli

--- res/templates/module/app/src/cli/$[name]-cli.ts
+++ res/templates/module/app/src/cli/$[name]-cli.ts
@@ -1,16 +1,16 @@
-#!/usr/bin/env node
-import $[name] from "../$[name]"
-import { program } from "commander"
-import reqPackageJson from "req-package-json"
-const config = reqPackageJson()
+// #!/usr/bin/env node
+// import $[name] from "../$[name]"
+// import { program } from "commander"
+// import reqPackageJson from "req-package-json"
+// const config = reqPackageJson()
 
-program
-  .version(config.version)
-  .name(config.name)
+// program
+//   .version(config.version)
+//   .name(config.name)
 
-program
-  .option('-s, --silent', 'silence stdout')
-.parse(process.argv)
+// program
+//   .option('-s, --silent', 'silence stdout')
+// .parse(process.argv)
 
 
-$[name](...program.args)
+// $[name](...program.args)
