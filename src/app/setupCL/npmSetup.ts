433176cdf10819ea8e06a33fbd614ed8bea0baca
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-02-25 00:02:28 +0100

    fix npm publish

--- src/app/setupCL/npmSetup.ts
+++ src/app/setupCL/npmSetup.ts
@@ -3,7 +3,7 @@ import exec, { check } from "./shell"
 export default async function(options) {
   check("npm")
 
-  if (options.publish ?? false) exec(`npm publish${options.npmOtp ? ` --otp ${options.npmOtp}` : ""}`)
+  if (options.public ?? false) exec(`npm publish${options.npmOtp ? ` --otp ${options.npmOtp}` : ""}`)
   
   // exec("npm i")
   // dependencies.ea((dependency) => {
