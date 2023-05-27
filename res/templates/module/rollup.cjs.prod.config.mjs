7e107a75a575d4b905824d1fa49850f77c016b32
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-05-27 17:05:41 +0200

    update mod build

---
+++ res/templates/module/rollup.cjs.prod.config.mjs
@@ -0,0 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonJS from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+
+
+export default {
+  input: './app/src/$[ name ].ts',
+  output: {
+    file: 'app/dist/cjs/$[ name ].js',
+    format: 'cjs',
+    sourcemap: true,
+    inlineDynamicImports: true
+  },
+  plugins: [
+    typescript({tsconfig: "./tsconfig.prod.cjs.json", noEmitOnError: false, sourceMap: true}), 
+    resolve({modulesOnly: true, preferBuiltins: true}),
+    commonJS({
+      include: 'node_modules/**'
+    }),
+    json()
+  ]
+};
