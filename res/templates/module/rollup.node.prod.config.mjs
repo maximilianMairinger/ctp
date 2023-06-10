eb2c81104a2b68ce600e6960ae9488ceaf407bd3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-06-10 14:35:09 +0200

    mod: fix oupsi in build

--- res/templates/module/rollup.node.prod.config.mjs
+++ res/templates/module/rollup.node.prod.config.mjs
@@ -5,7 +5,7 @@ import commonMod from "./rollup.node.common.config.mjs"
 export default merge(commonMod, {
   input: 'app/src/$[name].ts',
   output: {
-    file: 'dist/cjs/$[name].js',
+    file: 'app/dist/cjs/$[name].js',
     format: 'cjs'
   },
 })
