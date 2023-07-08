e9d75222863dcdc35e6f2fa6faac125722e2dfb5
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-07-08 17:19:43 +0200

    fix interpolateFile

--- src/app/lib/refactorFile/interpolateFile.ts
+++ src/app/lib/refactorFile/interpolateFile.ts
@@ -1,7 +1,12 @@
 import interpolate from "../interpolate/interpolate"
 import { promises as fs } from "fs"
+import isBinPath from 'is-binary-path';
+
+
+
 
 export default async function(at: string, options: GenericObject) {
+  if (isBinPath(at)) return;
   let s = (await fs.readFile(at)).toString();
   s = interpolate(s, options)
   await fs.writeFile(at, s);
