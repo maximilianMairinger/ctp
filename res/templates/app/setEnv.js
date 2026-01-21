9f934d3166ad1b58d85f1bad2f4ad9da5b3cadd7
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2026-01-21 22:27:17 +0100

    set env work

--- res/templates/app/setEnv.js
+++ res/templates/app/setEnv.js
@@ -3,15 +3,12 @@ const agrs = yargs.argv
 const preset = agrs.preset ? agrs.preset : "repl"
 const fs = require("fs")
 
-if (!fs.existsSync("ecosystem.config.js")) {
-  console.error("This is only allowed on the server")
-}
-else {
-  fs.writeFileSync(".deploy", preset)
 
-  if (fs.existsSync("pm2Reload.js")) {
-    require("./pm2Reload.js")
-  }
+
+fs.writeFileSync(".deploy", preset)
+
+if (fs.existsSync("pm2Reload.js")) {
+  require("./pm2Reload.js")
 }
 
 
